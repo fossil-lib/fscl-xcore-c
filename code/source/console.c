78193e1f39867a95fe672d39c9050cddacfc92fe
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-12-22 15:42:25 -0600

    Adding standard args

--- code/source/console.c
+++ code/source/console.c
@@ -33,11 +33,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h> // for usleep function
+#include <stdarg.h>
 
 #ifdef _WIN32
 #include <conio.h>
