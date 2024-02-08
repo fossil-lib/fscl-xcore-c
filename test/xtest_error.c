de7b57a242abec3867293185481b7e9dbd9cfba6
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-02-07 21:41:09 -0600

    Update xtest_error.c

--- test/xtest_error.c
+++ test/xtest_error.c
@@ -10,7 +10,7 @@ Description:
     feel free to contact Michael at michaelbrockus@gmail.com.
 ==============================================================================
 */
-#include "fossil/xcore/stream.h" // lib source code
+#include "fossil/xcore/error.h" // lib source code
 
 #include <fossil/xtest.h>   // basic test tools
 #include <fossil/xassert.h> // extra asserts
