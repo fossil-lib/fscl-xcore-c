3e2cb992dc5a19699dd3aba8f5ab7b89072fab0c
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-09 09:35:23 -0600

    Update thread.h

--- code/include/fossil/xcore/thread.h
+++ code/include/fossil/xcore/thread.h
@@ -62,30 +62,123 @@ typedef struct {
 #define cthread_task(name, arg) CTHREAD_TASK_RETURN_TYPE name(CTHREAD_TASK_ARG_TYPE arg)
 
 // =================================================================
-//  Classic Thread Management
+// Classic Thread Management
 // =================================================================
+
+/**
+ * Create a new thread.
+ *
+ * @param func The function to be executed by the thread.
+ * @param arg  The argument to be passed to the thread function.
+ * @return     A handle to the created thread.
+ */
 cthread fscl_thread_create(CThreadFunc func, void* arg);
+
+/**
+ * Wait for a thread to finish execution.
+ *
+ * @param handle The handle of the thread to join.
+ */
 void fscl_thread_join(cthread handle);
+
+/**
+ * Erase a thread and free associated resources.
+ *
+ * @param handle The handle of the thread to erase.
+ */
 void fscl_thread_erase(cthread handle);
+
+/**
+ * Sleep the current thread for a specified number of milliseconds.
+ *
+ * @param milliseconds The duration of sleep in milliseconds.
+ */
 void fscl_thread_sleep(unsigned int milliseconds);
+
+/**
+ * Yield the execution of the current thread to another thread.
+ */
 void fscl_thread_yield();
+
+/**
+ * Detach a thread, allowing it to run independently.
+ *
+ * @param handle The handle of the thread to detach.
+ */
 void fscl_thread_detach(cthread handle);
+
+/**
+ * Get the ID of the current thread.
+ *
+ * @return The ID of the current thread.
+ */
 unsigned long fscl_thread_get_id();
 
 // =================================================================
-//  Thread Pool Management
+// Thread Pool Management
 // =================================================================
+
+/**
+ * Create a new thread pool with a specified number of threads.
+ *
+ * @param num_threads The number of threads in the pool.
+ * @return            A handle to the created thread pool.
+ */
 cthread_pool fscl_thread_pool_create(int num_threads);
+
+/**
+ * Execute a task in the thread pool.
+ *
+ * @param pool The thread pool in which to execute the task.
+ * @param func The function to be executed by a thread in the pool.
+ * @param arg  The argument to be passed to the thread function.
+ */
 void fscl_thread_pool_execute(cthread_pool pool, CThreadFunc func, void* arg);
+
+/**
+ * Wait for all tasks in the thread pool to finish execution.
+ *
+ * @param pool The thread pool to wait for.
+ */
 void fscl_thread_pool_wait(cthread_pool pool);
+
+/**
+ * Erase a thread pool and free associated resources.
+ *
+ * @param pool The thread pool to erase.
+ */
 void fscl_thread_pool_erase(cthread_pool pool);
 
 // =================================================================
-// Mutex thread managment
+// Mutex Thread Management
 // =================================================================
+
+/**
+ * Create a new mutex.
+ *
+ * @param mutex Pointer to the cmutex structure to store the created mutex.
+ */
 void fscl_mutex_create(cmutex* mutex);
+
+/**
+ * Lock a mutex, preventing other threads from accessing the protected resource.
+ *
+ * @param mutex Pointer to the cmutex structure to be locked.
+ */
 void fscl_mutex_lock(cmutex* mutex);
+
+/**
+ * Unlock a locked mutex, allowing other threads to access the protected resource.
+ *
+ * @param mutex Pointer to the cmutex structure to be unlocked.
+ */
 void fscl_mutex_unlock(cmutex* mutex);
+
+/**
+ * Erase a mutex and free associated resources.
+ *
+ * @param mutex Pointer to the cmutex structure to be erased.
+ */
 void fscl_mutex_erase(cmutex* mutex);
 
 #ifdef __cplusplus
