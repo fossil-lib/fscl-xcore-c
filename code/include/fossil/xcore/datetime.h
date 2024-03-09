4b1ad6422662351969b91ec577a910bfc3a83108
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-09 09:19:59 -0600

    Update datetime.h

--- code/include/fossil/xcore/datetime.h
+++ code/include/fossil/xcore/datetime.h
@@ -42,23 +42,98 @@ typedef struct {
 // =================================================================
 // Timestamp functions
 // =================================================================
+
+/**
+ * Parse a military time string and populate a cdatetime structure with the current date.
+ *
+ * @param str      The military time string to be parsed.
+ * @param datetime Pointer to a cdatetime structure to store the parsed date and time.
+ * @return         True if parsing is successful, false otherwise.
+ */
 bool fscl_datetime_get_current_parse_military(const char *str, cdatetime *datetime);
+
+/**
+ * Parse a 12-hour time string and populate a cdatetime structure with the current date.
+ *
+ * @param str      The 12-hour time string to be parsed.
+ * @param datetime Pointer to a cdatetime structure to store the parsed date and time.
+ * @return         True if parsing is successful, false otherwise.
+ */
 bool fscl_datetime_get_current_parse_12_hour(const char *str, cdatetime *datetime);
+
+/**
+ * Parse a human-readable time string and populate a cdatetime structure with the current date.
+ *
+ * @param str      The human-readable time string to be parsed.
+ * @param datetime Pointer to a cdatetime structure to store the parsed date and time.
+ * @return         True if parsing is successful, false otherwise.
+ */
 bool fscl_datetime_get_current_parse_human_readable(const char *str, cdatetime *datetime);
+
+/**
+ * Get a custom string representation of a cdatetime structure.
+ *
+ * @param datetime Pointer to a cdatetime structure.
+ * @param str      Buffer to store the custom string representation.
+ * @param max_size Maximum size of the buffer.
+ */
 void fscl_datetime_get_current_custom(const cdatetime *datetime, char *str, size_t max_size);
 
 // =================================================================
 // Calander functions
 // =================================================================
+
+/**
+ * Get the current date in the "YYYY-MM-DD" format from a ccalendar structure.
+ *
+ * @param calendar Pointer to a ccalendar structure.
+ * @param str      Buffer to store the formatted date string.
+ * @param max_size Maximum size of the buffer.
+ */
 void fscl_datetime_get_current_calendar_yyyy_mm_dd(const ccalendar *calendar, char *str, size_t max_size);
+
+/**
+ * Get the current date in the "DD-MM-YYYY" format from a ccalendar structure.
+ *
+ * @param calendar Pointer to a ccalendar structure.
+ * @param str      Buffer to store the formatted date string.
+ * @param max_size Maximum size of the buffer.
+ */
 void fscl_datetime_get_current_calendar_dd_mm_yyyy(const ccalendar *calendar, char *str, size_t max_size);
+
+/**
+ * Get the current date in the "Month DD, YYYY" format from a ccalendar structure.
+ *
+ * @param calendar Pointer to a ccalendar structure.
+ * @param str      Buffer to store the formatted date string.
+ * @param max_size Maximum size of the buffer.
+ */
 void fscl_datetime_get_current_calendar_month_dd_yyyy(const ccalendar *calendar, char *str, size_t max_size);
+
+/**
+ * Check if the given year is a leap year.
+ *
+ * @param year The year to be checked.
+ * @return     True if the year is a leap year, false otherwise.
+ */
 bool fscl_datetime_get_current_is_leap_year(int year);
 
 // =================================================================
 // DateTime utilities
 // =================================================================
+
+/**
+ * Sleep for the specified number of seconds.
+ *
+ * @param seconds The duration of sleep in seconds.
+ */
 void fscl_datetime_sleep(unsigned int seconds);
+
+/**
+ * Get the current date and time and populate a cdatetime structure.
+ *
+ * @param datetime Pointer to a cdatetime structure to store the current date and time.
+ */
 void fscl_datetime_get_current(cdatetime *datetime);
 
 #ifdef __cplusplus
